548af24bc1563a81fb50e590cdda91a80f9ce85e
Aliaksei Mazurchyk <tortumine@gmail.com>
2017-10-01 12:04:44 +0200

    Update Median.h

--- Median.h
+++ Median.h
@@ -1,8 +1,12 @@
 /* -------------------------------------------------------------------- *
- * Draw a random array of int and returns its median
- *
- * RETURN
- * median            The median of the random array
- * --------------------------------------------------------------------*/
-int f(int, int, unsigned int*);
+* Draw a random array of int and returns its median
+*
+* RETURN
+* median            The median of the random array
+* --------------------------------------------------------------------*/
+#define uint unsigned int
+
+int f(int, int, uint*);
+void f2(int*, int*, int);
+void f1(int*);
 int median(void);
